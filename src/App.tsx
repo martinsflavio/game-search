28402631737161710bfd519b5308cd5ecda1828a
Flavio dos Reis Martins <flaviomartinsdev@gmail.com>
2023-04-02 20:39:09 -0400

    Project setup + ListGroup component.

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import ListGroup from './components/ListGroup';
+import './App.css'
+
+function App() {
+  return (
+    <div className="App">
+        <ListGroup />
+    </div>
+  )
+}
+
+export default App;
